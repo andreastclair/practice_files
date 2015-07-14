bc78fd001ed098acd51696eb8abdf0cc0126ceaa
Andrea St. Clair <andreadoris86@gmail.com>
2015-07-14 15:35:03 -0500

    tiny bit of js with aaron

---
+++ java-with-aaron/main.js
@@ -0,0 +1,12 @@
+$('#michael').on('click', tito);
+
+function tito() {
+	$('#francois').html('Francois v2');
+	$('#francois').css('color', '#bada55');
+}
+
+
+
+// function aaron () {
+// 	$('#francois').sitDown()
+// }
